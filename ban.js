07a68d64914f25c53a5b31567deeec01865cc50f
Nguyễn Quang Minh <97344872+peminh25@users.noreply.github.com>
2022-05-26 20:02:44 +0700

    Update ban.js

--- ban.js
+++ ban.js
@@ -2,7 +2,7 @@
 "100081846872229": {
         "reason": "An tồy quá nên ban =)))))",
         "dateAdded": "26/5/2022"
-    }
+    };
 "100081846872229": {
         "reason": "Test xíu :))",
         "dateAdded": "26/5/2022"
